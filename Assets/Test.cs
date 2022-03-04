727e17649c7f917abe23560a49a634f23f66086d
Axiejinhao <xjh9836@gamil.com>
2022-03-04 11:53:22 +0800

    Init

---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("test success");
+    }
+}
